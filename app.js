c96d9e8519e62d759e3d7e8b8de845f4235924e0
Payton Boone <paytonboone30@gmail.com>
2023-01-26 13:38:10 -0700

    Challenge Started

---
+++ app.js
@@ -0,0 +1,43 @@
+const heroes = [
+    {
+        name: 'Slate Slabrock',
+        type: 'dwarf',
+        damage: 5,
+        health: 100
+    },
+    {
+        name: 'Flint Ironstag',
+        type: 'elf',
+        damage: 10,
+        health: 50
+    }
+]
+
+
+const boss = {
+    health: 100,
+    maxHealth: 100,
+    damage: 5,
+    level: 1
+}
+
+
+
+function damageHeroes()
+function damageMonster()
+
+
+
+function reviveMonster()
+
+
+
+
+
+function buyPotion()
+function buySam()
+function buyJake()
+
+
+
+function resetGame()
